3e128bb8a2716fa3035d7beb48a7b71cea1d7d3b
Malena de Juan <malena.dj4@gmail.com>
2021-04-07 12:44:46 +0200

    funciones para crear error, mandar error y notFound

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const morgan = require("morgan");
 const { program } = require("commander");
 const facturas = require("./facturas.json");
+const { creaError, mandaErrores, notFoundError } = require("./utils/errores");
+const chalk = require("chalk");
 
 program.option("-p, --puerto <puerto>", "Puerto para el servidor");
 program.parse(process.arg);
@@ -16,6 +18,13 @@ const server = app.listen(puerto, () => {
   debug(`Servidor levantado en el puerto ${puerto}`);
 });
 
+server.on("error", err => {
+  debug(chalk.red.bold("Ha ocurrido un error en el servidor"));
+  if (err.code === "EADDRINUSE") {
+    debug(chalk.red.bold(`El puerto ${puerto} está ocupado.`));
+  }
+});
+
 app.use(morgan("dev"));
 app.get("/", (req, res, next) => {
   res.json(facturas);
@@ -23,5 +32,12 @@ app.get("/", (req, res, next) => {
 app.get("/factura/:id", (req, res, next) => {
   const { id } = req.params;
   let factura = facturas.facturas.find(factura => factura.id === +id);
-  res.json(factura);
+  if (factura) {
+    res.json(factura);
+  } else {
+    const error = creaError("La factura no existe", 404);
+    next(error);
+  }
 });
+app.use(notFoundError);
+app.use(mandaErrores);
