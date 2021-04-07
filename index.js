55fa1db6e29dff3a7130c775ad2d53648754417e
Malena de Juan <malena.dj4@gmail.com>
2021-04-07 12:15:43 +0200

    los get devuelven los objetos pedidos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const debug = require("debug")("facturas:root");
 const express = require("express");
 const morgan = require("morgan");
 const { program } = require("commander");
+const facturas = require("./facturas.json");
 
 program.option("-p, --puerto <puerto>", "Puerto para el servidor");
 program.parse(process.arg);
@@ -17,9 +18,10 @@ const server = app.listen(puerto, () => {
 
 app.use(morgan("dev"));
 app.get("/", (req, res, next) => {
-  res.json("Aquí iran las facturas");
+  res.json(facturas);
 });
 app.get("/factura/:id", (req, res, next) => {
   const { id } = req.params;
-  res.json(`Aquí irá la factura ${id}`);
+  let factura = facturas.facturas.find(factura => factura.id === +id);
+  res.json(factura);
 });
