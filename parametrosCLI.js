1ee12203da8199b63d9d3a4a56c9ce7be564dd24
Malena de Juan <malena.dj4@gmail.com>
2021-04-11 22:32:30 +0200

    añadida opcion datos a parametros

--- parametrosCLI.js
+++ parametrosCLI.js
@@ -1,6 +1,7 @@
 const { program } = require("commander");
 
 program.option("-p, --puerto <puerto>", "Puerto para el servidor");
+program.option("-d, --datos <datos>", "Elegir si los datos vienen de la base de datos o de archivo JSON");
 program.parse(process.arg);
 
 const options = program.opts();
